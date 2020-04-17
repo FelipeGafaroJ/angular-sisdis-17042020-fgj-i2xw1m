b66172d519d84a064fe3dc7a06adc7daf05278df
FelipeGafaroJ <luis.gafaro.2016@upb.edu.co>
2020-04-17 12:24:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sisdis-17042020-fgj-i2xw1m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sisdis-17042020-fgj-i2xw1m)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CartService {
+  items = [];
+  constructor(private http : HttpClient) { }
+
+// El addToCart()método agrega un producto a una matriz de items.
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+//  recopila los artículos que los usuarios agregan al carrito y devuelve cada artículo con su cantidad asociada.
+  getItems() {
+    return this.items;
+  }
+
+// devuelve una matriz vacía de elementos.
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+// para recuperar los datos de envío.
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+}
